efd32544d82463ea88a28c2d38c9288356a3e17b
daOrkz <fillippok87@gmail.com>
2022-04-25 22:03:28 +0700

    add slider

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+const gallery = document.querySelector('.gallery-img')
+const galleryIMG = document.getElementById('gallery-img')
 
+const img = ['_1.jpg','_2.jpg','_3.jpg','_4.jpg']
+
+let i = 0;
+function nextImg() {
+  if (i >= img.length) {
+    i = 0
+  }
+  let imgSrc = 'img/gallery/gallery'
+      
+  imgSrc += img[i]
+  galleryIMG.src = imgSrc
+  imgSrc = 'img/gallery/gallery'
+
+  i++
+}
+
+setInterval(nextImg, 3000)
